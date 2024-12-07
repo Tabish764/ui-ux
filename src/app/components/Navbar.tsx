2d39c629e6487954009baf595977b5460adf8e16
Tabish Raza <ts764raza@gmail.com>
2024-12-08 01:41:18 +0500

    first commit

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='mx-auto  justify-between border flex  px-6'>
+        <div className='flex list-none justify-between max-w-7xl  gap-5'>
+            <li>Hoem</li>
+            <li>Hoem</li>
+            <li>Hoem</li>
+            <li>Hoem</li>
+            <li>Hoem</li>
+        <div>
+            w
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
