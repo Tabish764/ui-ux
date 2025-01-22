a6fb727e3a6e2593ee51c4ef2fffc35746e2d030
Tabish Raza <ts764raza@gmail.com>
2025-01-22 14:09:45 +0500

    changing env

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -8,7 +8,7 @@ export const dataset = assertValue(
 
 export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+  'of6r4skk'
 )
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
